ba4b5ec4e02d3225a60ac89b538de725093faf45
Frank Li <frank.li.865985@gmail.com>
2025-11-03 18:43:21 -0500

    Initial Commit

---
+++ guidedcamera/guidedcameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  guidedcameraApp.swift
+//  guidedcamera
+//
+//  Created by Frank Li on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct guidedcameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guidedcameraTests/guidedcameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  guidedcameraTests.swift
+//  guidedcameraTests
+//
+//  Created by Frank Li on 11/3/25.
+//
+
+import Testing
+@testable import guidedcamera
+
+struct guidedcameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
